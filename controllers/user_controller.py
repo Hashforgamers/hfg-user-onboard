027100df8ff6c5e250ae084dd74a03c1f81068ea
bhanujoshi <jbhanu244@gmail.com>
2025-06-25 22:55:22 +0200

    Added Hash Coin :

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify, Blueprint, current_app
 from services.user_service import UserService
+from models.userHashCoin import UserHashCoin
 from services.referral_service import create_voucher_if_eligible
 
 user_blueprint = Blueprint('user', __name__)
@@ -54,5 +55,25 @@ def get_voucher_by_user(user_id):
         return jsonify({
             "vouchers": [v.to_dict() for v in vouchers],
         }), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+@user_blueprint.route('/users/<int:user_id>/hash-coins', methods=['GET'])
+def get_user_hash_coins(user_id):
+    try:
+        user_hash_coin = db.session.query(UserHashCoin).filter_by(user_id=user_id).first()
+
+        if not user_hash_coin:
+            return jsonify({
+                "user_id": user_id,
+                "hash_coins": 0,
+                "message": "No hash coins found for this user"
+            }), 200
+
+        return jsonify({
+            "user_id": user_id,
+            "hash_coins": user_hash_coin.hash_coins
+        }), 200
+
     except Exception as e:
         return jsonify({"error": str(e)}), 400
