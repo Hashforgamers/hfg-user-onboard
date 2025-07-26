5a5e66262cfbd9ac02593ee4c785d3a57521b65a
bhanujoshi <jbhanu244@gmail.com>
2025-07-26 17:01:35 +0200

    Added Pass feature

--- models/cafePass.py
+++ models/cafePass.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
+from db.extensions import db
 
 class CafePass(db.Model):
     __tablename__ = 'cafe_passes'
