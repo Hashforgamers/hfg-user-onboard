3f5a6b800b2390e2728ceb58d8d48f65bf9655ca
bhanujoshi <jbhanu244@gmail.com>
2025-07-27 20:08:10 +0200

    Added pass purchase

--- models/cafePass.py
+++ models/cafePass.py
@@ -5,12 +5,13 @@ from db.extensions import db
 class CafePass(db.Model):
     __tablename__ = 'cafe_passes'
     id = Column(Integer, primary_key=True)
-    vendor_id = Column(Integer)         # Null if hash pass
+    vendor_id = Column(Integer, ForeignKey('vendors.id'))         # Null if hash pass
     pass_type_id = Column(Integer, ForeignKey('pass_types.id'))   # Links to daily/monthly/yearly type
     name = Column(String(100), nullable=False)                    # e.g., "Platinum Pass", etc.
     price = Column(Float, nullable=False)
     days_valid = Column(Integer, nullable=False)                  # Number of days for the pass validity
     is_active = Column(Boolean, default=True)
     pass_type = relationship('PassType')
+    vendor = relationship('Vendor')
 
     description = Column(String(255))
---
+++ models/vendor.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship, foreign
+from db.extensions import db
+from datetime import datetime
+from sqlalchemy.sql import and_
+
+class Vendor(db.Model):
+    __tablename__ = 'vendors'
+    
+    id = Column(Integer, primary_key=True)
+    cafe_name = Column(String(255), nullable=False)
+    owner_name = Column(String(255), nullable=False)
+    description = Column(String(255), nullable=True)
+
+    # Timestamps
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __str__(self):
+        return f"Vendor(id={self.id}, cafe_name='{self.cafe_name}', owner_name='{self.owner_name}', description='{self.description}')"
+
+    def __repr__(self):
+        return self.__str__()
